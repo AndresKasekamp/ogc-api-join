156a8fff822bd1f76d57133762f722cbc0c79b6d
Andres Kasekamp <andreskasekamp@gmail.com>
2024-02-14 15:59:03 +0200

    settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    // Add basePath
+  basePath: '/github-pages',
+};
 
 export default nextConfig;

50540a2991b727ab0386e1e8ba2e5e55452eb84d
Andres Kasekamp <andreskasekamp@gmail.com>
2024-02-22 09:22:16 +0200

    reduced padding

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex p-24">
+        <main className="flex p-12">
           <GeometriesProvider>
             <BreaksProvider>
               {children}
